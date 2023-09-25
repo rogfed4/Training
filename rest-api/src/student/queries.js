b96b0f211a89f3ac6e93c52f42ef09a28559823d
rogfed4 <Anugureddy@4>
2023-09-25 19:04:28 +0530

    till sunday

--- rest-api/src/student/queries.js
+++ rest-api/src/student/queries.js
@@ -1,8 +1,17 @@
-const getStudents='SELECT * FROM person';
-const getStudentById='SELECt * FROM person WHERE id= $1';
-const checkEmailExists='SELECT * FROM person WHERE email=$1';
-const addStudent = 'INSERT INTO person (first_name,last_name,gender,date_of_birth,email) VALUES ($1,$2,$3,$4,$5)';
-
+const getStudentsQuery='SELECT * FROM student';
+const getStudentByIdQuery='SELECt * FROM student WHERE id= $1';
+const checkEmailExists='SELECT * FROM student WHERE email=$1';
+const checkId ='SELECT * FROM student WHERE id=$1';
+const addStudentQuery = 'INSERT INTO student (first_name,last_name,gender,date_of_birth,email, score) VALUES ($1,$2,$3,$4,$5,$6)';
+const removeStudentQuery='DELETE FROM student where id=$1';
+const getStudentsPercentageQuery='SELECT id,first_name,last_name,score, ROUND(score*100::NUMERIC / 360.0, 2) AS percentage, ROUND((SELECT AVG(score) FROM student),2) AS average_score FROM student';
+const updateStudentQuery='UPDATE student SET first_name= $1 , last_name= $2, gender=$3, date_of_birth=$4, email=$5, score=$6 WHERE id=$7';
 module.exports={
-    getStudents, getStudentById, checkEmailExists,addStudent
+    getStudentsQuery,
+    getStudentByIdQuery, 
+    checkId,checkEmailExists,
+    addStudentQuery,
+    removeStudentQuery,
+    getStudentsPercentageQuery,
+    updateStudentQuery
 };
--- rest-api/src/student/routes.js
+++ rest-api/src/student/routes.js
@@ -3,9 +3,15 @@ const controller= require('./controller.js');
 
 const router=Router();
 
+router.get('/percentage',controller.getStudentsPercentage);
+
 router.get('/', controller.getStudents);
 router.get('/:id',controller.getStudentById);
-router.post('/',controller.addStudent);
+router.post('/add',controller.addStudent);
+router.delete('/:id',controller.removeStudent);
+
+router.put('/:id',controller.updateStudent);
+
 
 
 module.exports= router;
